45ae95e4ab0eebc8ba0e87c8f4dcec68650bd082
Vahe Sevachyan <vscode13@gmail.com>
2026-03-07 11:11:35 -0800

    updated App.jsx styled components

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,43 @@ import "./App.css";
 import MainDisplay from "./layouts/MainDisplay";
 import NavBar from "./layouts/NavBar";
 import SideBar from "./layouts/SideBar";
+import styled from "styled-components";
+
+const UIContainer_ = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+const DashBoardWrapper_ = styled.div`
+  display: flex;
+  width: 1200px;
+  height: 800px;
+`;
+const SideBarWrapper_ = styled.section`
+  background-color: rgb(63, 170, 236);
+  max-width: 20%;
+  min-width: 150px;
+`;
+const MainDisplayWrapper_ = styled.section`
+  width: 100%;
+  background-color: rgb(77, 135, 135);
+`;
 
 function App() {
   return (
-    <div className="main-wrapper">
-      <div className="dashboard-container">
-        <section className="sidebar-wrapper">
+    <UIContainer_>
+      <DashBoardWrapper_>
+        <SideBarWrapper_>
           <SideBar />
-        </section>
+        </SideBarWrapper_>
 
-        <section className="main-content">
+        <MainDisplayWrapper_>
           <NavBar className="navbar-child" />
           <MainDisplay className="main-display-wrapper" />
-        </section>
-      </div>
-    </div>
+        </MainDisplayWrapper_>
+      </DashBoardWrapper_>
+    </UIContainer_>
   );
 }
 
